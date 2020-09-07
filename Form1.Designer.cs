7a3924c78829f8a58804b5ca9b9676f39ea3ed2e
jack-debug <55571956+jack-debug@users.noreply.github.com>
2020-09-07 14:24:30 +1000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ComputerCrashingTools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.crash = new System.Windows.Forms.Button();
+            this.shutdown = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(518, 56);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Windows Crash Tools";
+            // 
+            // crash
+            // 
+            this.crash.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.crash.Location = new System.Drawing.Point(22, 92);
+            this.crash.Name = "crash";
+            this.crash.Size = new System.Drawing.Size(153, 65);
+            this.crash.TabIndex = 1;
+            this.crash.Text = "Press For Crash";
+            this.crash.UseVisualStyleBackColor = true;
+            this.crash.Click += new System.EventHandler(this.crash_Click);
+            // 
+            // shutdown
+            // 
+            this.shutdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.shutdown.Location = new System.Drawing.Point(359, 92);
+            this.shutdown.Name = "shutdown";
+            this.shutdown.Size = new System.Drawing.Size(153, 65);
+            this.shutdown.TabIndex = 2;
+            this.shutdown.Text = "Press For Shutdown";
+            this.shutdown.UseVisualStyleBackColor = true;
+            this.shutdown.Click += new System.EventHandler(this.shutdown_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 177);
+            this.Controls.Add(this.shutdown);
+            this.Controls.Add(this.crash);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Widnows Crash Tools";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button crash;
+        private System.Windows.Forms.Button shutdown;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComputerCrashingTools
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void crash_Click(object sender, EventArgs e)
+        {
+            fork();
+        }
+
+        private void shutdown_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo startinfo = new ProcessStartInfo("shutdown.exe", "-s");
+            Process.Start(startinfo);
+        }
+        public static void fork()
+        {
+            while (true)
+            {
+                fork();
+            }
+        }
+    }
+}
